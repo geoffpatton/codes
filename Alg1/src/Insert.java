b24c3cafe88943acc449dcfda049e95e510ed29f
Geoff Patton <geoffpatton@gmail.com>
2015-06-15 20:01:10 -0400

    6/15

--- Alg1/src/Insert.java
+++ Alg1/src/Insert.java
@@ -24,20 +24,52 @@ public class Insert {
         System.out.println(randomList);
 
        /* System.out.println("now run insertSort");
-
         List<Integer> sortList = insertSort(randomList);
-
         System.out.println(sortList);
+--
+        System.out.println("now run quickSort");
+        List<Integer> sortList2 = quicksort(randomList);
+        System.out.println(sortList2);
 */
 
-        System.out.println("now run quickSort");
+        System.out.println("now run mergesort");
+        List<Integer> sortList3 = mergesort(randomList);
+        System.out.println(sortList3);
+    }
 
-        List<Integer> sortList2 = quicksort(randomList);
-
-        System.out.println(sortList2);
-
-
-
+    public static List<Integer> mergesort(final List<Integer> values) {
+        if (values.size() < 2) {
+            return values;
+        }
+        final List<Integer> leftHalf =
+                values.subList(0, values.size() / 2);
+        final List<Integer> rightHalf =
+                values.subList(values.size() / 2, values.size());
+        return merge(mergesort(leftHalf), mergesort(rightHalf));
+    }
+    private static List<Integer> merge(final List<Integer> left,
+                                       final List<Integer> right) {
+        int leftPtr = 0;
+        int rightPtr = 0;
+        final List<Integer> merged = new ArrayList<>(left.size() + right.size());
+        while (leftPtr < left.size() && rightPtr < right.size()) {
+            if (left.get(leftPtr) < right.get(rightPtr)) {
+                merged.add(left.get(leftPtr));
+                leftPtr++;
+            } else {
+                merged.add(right.get(rightPtr));
+                rightPtr++;
+            }
+        }
+        while (leftPtr < left.size()) {
+            merged.add(left.get(leftPtr));
+            leftPtr++;
+        }
+        while (rightPtr < right.size()) {
+            merged.add(right.get(rightPtr));
+            rightPtr++;
+        }
+        return merged;
     }
 
     public static List<Integer> quicksort(List<Integer> numbers) {
---
+++ Override/src/Animal.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tua26762 on 6/15/2015.
+ */
+public class Animal {
+    public static void testClassMethod() {
+        System.out.println("The static method in Animal");
+    }
+    public void testInstanceMethod() {
+        System.out.println("The instance method in Animal");
+    }
+}
