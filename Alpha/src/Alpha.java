5a5f1449c3861b73bfacff7a6d050b5a7d7ef9d8
geoffpatton <geoffpatton@gmail.com>
2015-06-23 10:20:35 -0400

    6/23 test

---
+++ Alpha/src/Alpha.java
@@ -0,0 +1,15 @@
+/**
+ * Created by geoffpatton on 6/23/15.
+ */
+public class Alpha {
+
+    public Alpha(){
+    }
+
+    public static void main(String[] args){
+
+        System.out.print("Yolo");
+    }
+
+
+}
