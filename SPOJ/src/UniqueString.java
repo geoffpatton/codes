c15acaa185c71fed77dde23d19e63d06e39b9f9a
Geoff Patton <geoffpatton@gmail.com>
2015-06-24 15:35:28 -0400

    6/24

---
+++ SPOJ/src/UniqueString.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by tua26762 on 6/24/2015.
+ */
+public class UniqueString {
+
+    public static void main(String[] args){
+
+        Set<String> uniques = new HashSet<>();
+        Set<String> dups = new HashSet<>();
+
+        for(String s : args){
+            if(!uniques.add(s)){
+                dups.add(s);
+            }
+        }
+
+        uniques.removeAll(dups);
+
+        System.out.println(uniques.size());
+
+    }
+
+}
