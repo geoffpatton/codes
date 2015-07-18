3b7bb66bc93fbdb6bb4f52970ea5621609ab2790
Geoff Patton <geoffpatton@gmail.com>
2015-07-18 14:11:14 -0400

    7/18/15

--- SPOJ/src/ArraySub.java
+++ SPOJ/src/ArraySub.java
@@ -1,55 +1,52 @@
-import java.util.Arrays;
-import java.util.OptionalInt;
-import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Created by geoffpatton on 7/17/2015.
  * ARRAYSUB - subarrays
  */
 public class ArraySub {
-
-    public static void main(String[] args){
+    private static ReadInput in;
+    public static void main(String[] args) throws IOException {
         new ArraySub().run();
     }
-
-    private void run(){
-        Scanner in = new Scanner(System.in);
-
+    public void run() throws IOException {
+        in = new ReadInput ();
         int size = in.nextInt();
         int[] array = new int[size];
         for (int i  = 0; i<size;i++){
             array[i] = in.nextInt();
         }
         int subSize = in.nextInt();
-        int[] subArray = new int[subSize];
-        int lowestMax = 0;
-
-
         for (int j = 0; j < size - subSize+1; j++){
+            int largest = Integer.MIN_VALUE;
             for(int y= 0; y < subSize; y++){
-                subArray[y] = array[j+y];
-            }
-            //System.out.println(Arrays.toString(subArray));
-            OptionalInt highest = Arrays.stream(subArray).max();
-            int subMax = highest.getAsInt();
-            if(j==0) {
-                lowestMax = subMax;
-              //  System.out.println(lowestMax);
-            }else{
-                if(lowestMax > subMax){
-                    lowestMax = subMax;
-                //    System.out.println(lowestMax);
+                if(array[y+j]>largest){
+                    largest = array[y+j];
                 }
             }
+            System.out.print(largest);
+            if(j<size - subSize){
+                System.out.print(" ");
+            }
         }
-
-
-        System.out.println(lowestMax);
-
-
-
     }
-
-
-
 }
+class ReadInput {
+    final private int BUFFER_SIZE = 1 << 16;private DataInputStream din;private byte[] buffer;private int bufferPointer, bytesRead;
+    public ReadInput(){din=new DataInputStream(System.in);buffer=new byte[BUFFER_SIZE];bufferPointer=bytesRead=0;}
+    public ReadInput(String file_name)throws IOException{din=new DataInputStream(new FileInputStream(file_name));buffer=new byte[BUFFER_SIZE];bufferPointer=bytesRead=0;}
+    public String readLine()throws IOException{byte[] buf=new byte[1024];int cnt=0,c;
+        while((c=read())!=-1){if(c=='\n')break;buf[cnt++]=(byte)c;}return new String(buf,0,cnt);}
+    public int nextInt()throws IOException{int ret=0;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');
+        if(neg)c=read();do{ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(neg)return -ret;return ret;}
+    public long nextLong()throws IOException{long ret=0;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');
+        if(neg)c=read();do{ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(neg)return -ret;return ret;}
+    public double nextDouble()throws IOException{double ret=0,div=1;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');if(neg)c = read();do {ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');
+        if(c=='.')while((c=read())>='0'&&c<='9')ret+=(c-'0')/(div*=10);if(neg)return -ret;return ret;}
+    private void fillBuffer()throws IOException{bytesRead=din.read(buffer,bufferPointer=0,BUFFER_SIZE);if(bytesRead==-1)buffer[0]=-1;}
+    private byte read()throws IOException{if(bufferPointer==bytesRead)fillBuffer();return buffer[bufferPointer++];}
+    public void close()throws IOException{if(din==null) return;din.close();}
+}
+
