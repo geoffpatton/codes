5527d4b4156a9395f8632360c9c37bf31a1e0f61
Geoff Patton <geoffpatton@gmail.com>
2015-07-23 18:07:26 -0400

    7/23/15

---
+++ SPOJ/src/Ndiv.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Created by geoffpatton on 7/23/2015.
+ * NDIV - n-divisors
+ */
+public class Ndiv {
+    public static void main(String[] args){
+        new Ndiv().run();
+    }
+    private void run(){
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int n = in.nextInt();
+
+        int count=0;
+
+        /*for(int i = a; i<=b; i++){
+            for(y =)
+
+        }
+*/
+
+
+    }
+}
