cc0e3eda0df4c5b8982409c0899bbcab99db0da3
Geoff Patton <geoffpatton@gmail.com>
2015-07-24 15:30:41 -0400

    7/24/15

--- SPOJ/src/Ndiv.java
+++ SPOJ/src/Ndiv.java
@@ -1,28 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * Created by geoffpatton on 7/23/2015.
  * NDIV - n-divisors
  */
 public class Ndiv {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         new Ndiv().run();
     }
-    private void run(){
-        Scanner in = new Scanner(System.in);
+    private void run() throws IOException {
+        //Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer in = new StringTokenizer(br.readLine());
+        int limit = 1000000000;
+        int size = 100005;
+        int[] cnt = new int[size];
 
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int n = in.nextInt();
+        int a = Integer.parseInt(in.nextToken());
+        int b = Integer.parseInt(in.nextToken());
+        int n = Integer.parseInt(in.nextToken());
 
-        int count=0;
-
-        /*for(int i = a; i<=b; i++){
-            for(y =)
-
+        for (int i = 1; i * i <= limit; i++) { //  seive
+            int j = (int) Math.ceil(1.0 * a / i) * i;
+            for (; j <= b; j += i) {
+                int check = j / i;
+                if (check == i) // if sqrt only counts once
+                    cnt[j-a]++;
+                else if (check > i) // ad 2 if each no have k and n/k as its factors ,
+                    cnt[j-a] += 2;
+            }
         }
-*/
-
-
+        int ans = 0;
+        int addup = b - a;
+        for (int i = 0; i <= addup; i++) {
+            if (cnt[i] == n)
+                ans++;
+        }
+        System.out.print(ans);
     }
 }
