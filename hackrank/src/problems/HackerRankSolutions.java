5f0fff0c77d7d4b11467d88a06a071351bfca27c
Geoff <geoffpatton@gmail.com>
2019-11-11 16:57:26 -0500

    added a few more hackerrank solutions

--- hackrank/src/problems/HackerRankSolutions.java
+++ hackrank/src/problems/HackerRankSolutions.java
@@ -30,4 +30,121 @@ class HackerRankSolutions {
         return jump;
     }
 
+    /**
+     * Find the maximum hour glass sum from the 6x6 array
+     * 
+     * @param arr
+     * @return largest (maximum) hourglass sum found in the given array
+     */
+    int hourglassSum(int[][] arr) {
+        int colLen = arr.length;
+        int rowLen = arr[0].length;
+
+        int maxSum = Integer.MIN_VALUE; // set Max Sum to Integer Min value
+        for (int i = 0; i < colLen - 2; ++i) {
+            for (int y = 0; y < rowLen - 2; ++y) {
+                maxSum = Math.max(maxSum, arr[i][y] + arr[i][y + 1] + arr[i][y + 2] + arr[i + 1][y + 1] + arr[i + 2][y]
+                    + arr[i + 2][y + 1] + arr[i + 2][y + 2]);
+            }
+        }
+        return maxSum;
+    }
+
+    /**
+     * Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm above.
+     * Once sorted, print the following three lines:
+     * <p>
+     * Array is sorted in (count) swaps.
+     * First Element: a[0]
+     * Last Element: a[len -1]
+     * 
+     * @param a array of integers
+     * @return number of swaps. added for testing
+     */
+    int countSwaps(int[] a) {
+        int len = a.length;
+        int swaps = 0;
+        int tmp = 0;
+
+        for (int i = 0; i < len; ++i) {
+            for (int j = 0; j < len - 1; ++j) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                    ++swaps;
+                }
+            }
+        }
+        System.out.println("Array is sorted in " + swaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[len - 1]);
+
+        return swaps;
+    }
+
+    /**
+     * Given a string, determine how many special substrings can be formed from it.
+     * 
+     * @param n an integer, the length of string s
+     * @param s String
+     * @return count of total special substrings.
+     */
+    long substrCount(int n, String s) {
+        long count = 0;
+
+        // when all letters are the same
+        int index;
+        for (int i = 0; i < n; ++i) {
+            index = i;
+            while (index < n && s.charAt(i) == s.charAt(index)) {
+                ++index;
+            }
+            count += index - i;
+        }
+
+        int prevIndex;
+        int nextIndex;
+        char first;
+        // when middle letter is different
+        for (int i = 1; i < n; ++i) {
+            index = 1;
+            nextIndex = i + index;
+            prevIndex = i - index;
+            first = s.charAt(prevIndex);
+            while (prevIndex >= 0 && nextIndex < n && s.charAt(prevIndex) == s.charAt(nextIndex)
+                && first == s.charAt(nextIndex) && s.charAt(nextIndex) != s.charAt(i)) {
+                ++index;
+                nextIndex = i + index;
+                prevIndex = i - index;
+            }
+            count += index - 1;
+        }
+        return count;
+    }
+
+    /**
+     * Given an array of integers: a, and a number: d, perform d left rotations on the array.
+     * Return the updated array to be printed as a single line of space-separated integers.
+     * 
+     * @param a array of integer
+     * @param d number of left rotations to perform
+     * @return the rotated array
+     */
+    int[] rotLeft(int[] a, int d) {
+        int len = a.length;
+        int end; // end index location
+        int[] result = new int[len]; // new array to hold the result
+
+        // iterate over each element
+        for (int x = 0; x < len; ++x) {
+            end = x - d; // find the end location of the element
+            if (end < 0) {
+                end += len;
+            }
+            result[end] = a[x]; // place element in the result array
+        }
+        return result;
+    }
 }
--- hackrank/test/problems/HackerRankTests.java
+++ hackrank/test/problems/HackerRankTests.java
@@ -1,6 +1,9 @@
 package problems;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,6 +28,74 @@ public class HackerRankTests {
         assertEquals(3, target.jumpingOnClouds(case4));
         int[] case5 = { 0, 0 };
         assertEquals(1, target.jumpingOnClouds(case5));
+    }
+
+    @Test
+    public void testHourGlassSum() {
+        int[][] case1 = { { 1, 1, 1, 0, 0, 0 }, // 1 1 1 0 0 0
+            { 0, 1, 0, 0, 0, 0 },  // 0 1 0 0 0 0
+            { 1, 1, 1, 0, 0, 0 }, // 1 1 1 0 0 0
+            { 0, 0, 2, 4, 4, 0 }, // 0 0 2 4 4 0
+            { 0, 0, 0, 2, 0, 0 }, // 0 0 0 2 0 0
+            { 0, 0, 1, 2, 4, 0 } }; // 0 0 1 2 4 0
+        assertEquals(19, target.hourglassSum(case1));
+
+        int[][] case2 = { { -9, -9, -9, 1, 1, 1 }, // -9 -9 -9 1 1 1
+            { 0, -9, 0, 4, 3, 2 },  // 0 -9 0 4 3 2
+            { -9, -9, -9, 1, 2, 3 }, // -9 -9 -9 1 2 3
+            { 0, 0, 8, 6, 6, 0 }, // 0 0 8 6 6 0
+            { 0, 0, 0, -2, 0, 0 }, // 0 0 0 -2 0 0
+            { 0, 0, 1, 2, 4, 0 } }; // 0 0 1 2 4 0
+        assertEquals(28, target.hourglassSum(case2));
+
+        int[][] case3 = { { 1, 1, 1, 0, 0, 0 }, // 1 1 1 0 0 0
+            { 0, 1, 0, 0, 0, 0 },  // 0 1 0 0 0 0
+            { 1, 1, 1, 0, 0, 0 }, // 1 1 1 0 0 0
+            { 0, 9, 2, -4, -4, 0 }, // 0 9 2 -4 -4 0
+            { 0, 0, 0, -2, 0, 0 }, // 0 0 0 -2 0 0
+            { 0, 0, -1, -2, -4, 0 } }; // 0 0 -1 -2 -4 0
+        assertEquals(13, target.hourglassSum(case3));
+
+        int[][] case4 = { { -1, -1, 0, -9, -2, -2 }, // -1 -1 0 -9 -2 -2
+            { -2, -1, -6, -8, -2, -5 },  // -2 -1 -6 -8 -2 -5
+            { -1, -1, -1, -2, -3, -4 }, // -1 -1 -1 -2 -3 -4
+            { -1, -9, -2, -4, -4, -5 }, // -1 -9 -2 -4 -4 -5
+            { -7, -3, -3, -2, -9, -9 }, // -7 -3 -3 -2 -9 -9
+            { -1, -3, -1, -2, -4, -5 } }; // -1 -3 -1 -2 -4 -5
+        assertEquals(-6, target.hourglassSum(case4));
+
+    }
+
+    @Test
+    public void testCountSwaps() {
+        int[] case1 = { 0, 1, 2 };
+        assertEquals(0, target.countSwaps(case1));
+        int[] case2 = { 1, 2, 3 };
+        assertEquals(0, target.countSwaps(case2));
+        int[] case3 = { 3, 2, 1 };
+        assertEquals(3, target.countSwaps(case3));
+    }
+
+    @Test
+    public void testSubStringCount() {
+        assertEquals(7, target.substrCount(5, "asasd"));
+        assertEquals(10, target.substrCount(4, "aaaa"));
+        assertEquals(12, target.substrCount(8, "mnonopoo"));
+        assertEquals(10, target.substrCount(7, "abcbaba"));
+    }
+
+    @Test
+    public void testRotLeft() {
+        int[] case1 = { 1, 2, 3, 4, 5 };
+        int[] result1 = { 5, 1, 2, 3, 4 };
+
+        assertTrue(Arrays.equals(result1, target.rotLeft(case1, 4)));
+
+        int[] case2 = { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97 };
+        int[] result2 = { 87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60 };
+
+        target.rotLeft(case2, 13);
+        assertTrue(Arrays.equals(result2, target.rotLeft(case2, 13)));
     }
 
 }
